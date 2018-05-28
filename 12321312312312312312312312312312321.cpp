9bd2cd86f5115628a9e84bcbac353fd10d7cb6ba
q8323816 <39685143+q8323816@users.noreply.github.com>
2018-05-28 11:52:49 +0800

    Add files via upload

---
+++ 12321312312312312312312312312312321.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace std;
+template<class T>
+void outputArray(const T *array,int count) 
+{
+   for (int i=0;i<count;i++)
+    cout<<Araay[i]<<" ";
+     cout<<endl;
+}
+int main()
+{
+	const int A=8,B=8,C=20;
+}
